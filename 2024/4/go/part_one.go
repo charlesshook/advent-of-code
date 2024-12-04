5eb7164d6ed913080ae61f6925e16ab3f2eabaf2
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-12-03 21:06:33 -0700

    Setup

---
+++ 2024/4/go/part_one.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"bufio"
+)
+
+func partOne(scanner *bufio.Scanner) int {
+	return 0
+}
---
+++ 2024/4/go/part_two.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"bufio"
+)
+
+func partTwo(scanner *bufio.Scanner) int {
+	return 0
+}
