ef071e19f98e9e716e0d5ff7cc1cc1d932b8e752
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-12-05 20:52:11 -0700

    Something is being calculated wrong

--- 2024/4/go/part_two.go
+++ 2024/4/go/part_two.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 )
 
 func partTwo(scanner *bufio.Scanner) int {
-	return 0
+	var charArray [][]rune
+	words := []string{"MAS"}
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		charArray = append(charArray, []rune(line))
+	}
+
+	moves := solve(charArray, words)
+
+	count := 0
+
+	for i := range len(moves) {
+		for j := i + 1; j < len(moves); j++ {
+			if pointsIntersect(moves[i][0], moves[i][1], moves[j][0], moves[j][1]) {
+				fmt.Println(moves[i][0], moves[i][1], moves[j][0], moves[j][1])
+				count += 1
+			}
+		}
+	}
+
+	return count
 }
--- 2024/4/go/utils.go
+++ 2024/4/go/utils.go
@@ -10,3 +10,137 @@ func print2DArray(charArray [][]rune) {
 		fmt.Println()
 	}
 }
+
+type Point struct {
+	x, y int
+}
+
+type Node struct {
+	children map[rune]*Node
+	isWord   bool
+}
+
+type Trie struct {
+	root *Node
+}
+
+func newTrie() *Trie {
+	return &Trie{root: &Node{children: make(map[rune]*Node)}}
+}
+
+func (t *Trie) insert(word string) {
+	node := t.root
+	for _, r := range word {
+		if _, ok := node.children[r]; !ok {
+			node.children[r] = &Node{children: make(map[rune]*Node)}
+		}
+		node = node.children[r]
+	}
+	node.isWord = true
+}
+
+func (t *Trie) search(word string) bool {
+	node := t.root
+
+	for _, r := range word {
+		if _, ok := node.children[r]; !ok {
+			return false
+		}
+
+		node = node.children[r]
+	}
+
+	return node.isWord
+}
+
+func solve(grid [][]rune, words []string) [][]Point {
+	n, m := len(grid), len(grid[0])
+	trie := newTrie()
+	moves := [][]Point{}
+	for _, word := range words {
+		trie.insert(word)
+	}
+
+	Points := []Point{
+		{0, 1}, {0, -1}, {1, 0}, {-1, 0}, {1, 1}, {1, -1}, {-1, 1}, {-1, -1},
+	}
+
+	for i := range n {
+		for j := range m {
+			if _, ok := trie.root.children[grid[i][j]]; ok {
+				for _, Point := range Points {
+					check(grid, trie.root, i, j, Point.x, Point.y, &moves)
+				}
+			}
+		}
+	}
+
+	return moves
+}
+
+func check(grid [][]rune, trie *Node, i int, j int, xPoint int, yPoint int, moves *[][]Point) {
+	n, m := len(grid), len(grid[0])
+	node := trie
+
+	startingI, startingJ := i, j
+
+	for i >= 0 && i < n && j >= 0 && j < m {
+		char := grid[i][j]
+
+		if nextNode, ok := node.children[char]; ok {
+			node = nextNode
+
+			// just append all the points to make the line
+
+			if node.isWord {
+				*moves = append(*moves, []Point{{startingI, startingJ}, {i, j}})
+			}
+		} else {
+			break
+		}
+
+		i += xPoint
+		j += yPoint
+	}
+}
+
+func onSegment(p Point, q Point, r Point) bool {
+	if q.x <= max(p.x, r.x) && q.x >= min(p.x, r.x) && q.y <= max(p.y, r.y) && p.y >= min(p.y, r.y) {
+		return true
+	}
+
+	return false
+}
+
+func orientation(p Point, q Point, r Point) int {
+	val := (q.y-p.y)*(r.x-q.x) - (q.x-p.x)*(r.y-q.y)
+
+	if val == 0 {
+		return 0
+	} else if val > 0 {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+func pointsIntersect(p1 Point, q1 Point, p2 Point, q2 Point) bool {
+	o1 := orientation(p1, q1, p2)
+	o2 := orientation(p1, q1, q2)
+	o3 := orientation(p2, q2, p1)
+	o4 := orientation(p2, q2, q1)
+
+	if o1 != o2 && o3 != o4 {
+		return true
+	} else if o1 == 0 && onSegment(p1, p2, q1) {
+		return true
+	} else if o2 == 0 && onSegment(p1, q2, q1) {
+		return true
+	} else if o3 == 0 && onSegment(p2, p1, q2) {
+		return true
+	} else if o4 == 0 && onSegment(p2, q1, q2) {
+		return true
+	} else {
+		return false
+	}
+}
