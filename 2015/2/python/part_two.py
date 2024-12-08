0a94cbfc97364aa99f02ae9306f3d3a7fd74fb17
Charles Shook <31199031+charlesshook@users.noreply.github.com>
2024-12-08 08:20:26 -0700

    2015 day 2 part 2

--- 2015/2/python/part_two.py
+++ 2015/2/python/part_two.py
@@ -1,2 +1,16 @@
 def partTwo(input):
-    inputString = input.readline()
+    l = 0
+    w = 0
+    h = 0
+    totalLengthOfRibbon = 0
+
+    for line in input:
+        temp = line.rstrip()
+        items = temp.split("x")
+        sides = [int(items[0]), int(items[1]), int(items[2])]
+        sides.sort()
+        totalLengthOfRibbon += (
+            2 * sides[0] + 2 * sides[1] + sides[0] * sides[1] * sides[2]
+        )
+
+    return totalLengthOfRibbon
